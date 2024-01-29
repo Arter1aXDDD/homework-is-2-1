2dc08662f42b2b91f778fd72c4944017ef96cab9
Arter1aXDDD <steepnikita64@gmail.com>
2024-01-29 11:04:54 +0300

    Написана программа исчисляющая градусы и индекс, что выводит результат

---
+++ task_2home/main.js
@@ -0,0 +1,9 @@
+let temp = 17
+if (temp>30) {
+    console.log('Можно надеть легкую одежду')
+} else if (temp > 20) {
+    console.log('Наденьте тёплую одежду')
+} else {
+    console.log('Наденьте зимнюю одежду')
+}
+
