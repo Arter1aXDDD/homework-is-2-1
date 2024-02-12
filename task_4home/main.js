c79d8ae714d91ab28ccb825518b36039774f54e4
Arter1aXDDD <steepnikita64@gmail.com>
2024-02-12 11:12:11 +0300

    Доработка к заданию 1

--- task_4home/main.js
+++ task_4home/main.js
@@ -1,9 +1,15 @@
 const list = [ [4, 3], [6, 2], [8,1] ]
-var sum = 0
+
+for (let item of list) {
+    console.log(`[${item}] количество элементов в массиве = ${item.length}`)
+}
+
+let sum = 0
 
 for (let i in list) {
     for (let j in list[i]) {
-        sum+=list[i][j]
+        sum += list[i][j];
     }
+    
 }
-console.log(sum)
+console.log(`Сумма всех элементов двумерного массива') = ${sum}`)
