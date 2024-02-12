889bdd2b10f6fc449e2627bbc122a8e5dbf4bf1f
Arter1aXDDD <steepnikita64@gmail.com>
2024-02-12 11:38:24 +0300

    Выполнили работу со сменой свойства

--- task_4home/main.js
+++ task_4home/main.js
@@ -1,15 +1,10 @@
-const list = [ [4, 3], [6, 2], [8,1] ]
 
-for (let item of list) {
-    console.log(`[${item}] количество элементов в массиве = ${item.length}`)
+const list_2 = [{name:'Alexey', surname:'Boston', duty:'любит'}, {name:'Dmitry', surname: 'Zalozny', duty: 'любит'}]
+
+for (let item of list_2) {
+    console.log(`${item.name} ${item.surname} ${item.duty} заведовать пабликом`)
+    console.log('А также')
+    item.duty = 'не любит'
+    console.log(`${item.name} ${item.surname} ${item.duty} флудить в чате`)
+    console.log(' ')
 }
-
-let sum = 0
-
-for (let i in list) {
-    for (let j in list[i]) {
-        sum += list[i][j];
-    }
-    
-}
-console.log(`Сумма всех элементов двумерного массива') = ${sum}`)
